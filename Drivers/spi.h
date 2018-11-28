67135efcd5c5d5d9076aef46c555b1be086bf856
francesco <francesco@ubuntu.usb>
2018-11-28 01:39:55 +0100

    Struct Reorder, SPI refactor

--- Drivers/spi.h
+++ Drivers/spi.h
@@ -7,11 +7,19 @@
 #define BCM2835_SPI0_BASE    			(BCM2835_PERI_BASE + 0x204000)
 
 // SPI0 GPIO Pins
-#define SPI0_CLK        11
-#define SPI0_MOSI       10
-#define SPI0_MISO       9
-#define SPI0_CE0        8
-#define SPI0_CE1        7
+#define SPI0_CLK            11
+#define SPI0_MOSI           10
+#define SPI0_MISO           9
+#define SPI0_CE0            8
+#define SPI0_CE1            7
+
+#define SPI0_CLEAR_FIFO_TX  0x10
+#define SPI0_CLEAR_FIFO_RX  0x20
+
+
+void spi0_init(void);
+void spi0_close(void);
+void spi0_set_clock_divider(uint16_t divider);
 
 
 #endif
